db1be71cf2c3c37f8a64c15c7450b1f15eb9dbd8
RealityMachina <tomclancy1995@gmail.com>
2018-05-09 10:44:13 -0400

    Initial code.

--- StabilityKnockdowns/InitClass.cs
+++ StabilityKnockdowns/InitClass.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Harmony;
+using System.Reflection;
 
 namespace StabilityKnockdowns
 {
     public class InitClass
     {
+        public static void Init()
+        {
+            var harmony = HarmonyInstance.Create("Battletech.realitymachina.MoreKnockdowns");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
     }
 }
---
+++ StabilityKnockdowns/KnockdownChecker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using BattleTech;
+using System.Configuration;
+
+namespace StabilityKnockdowns
+{
+    class KnockdownChecker
+    {
+        [HarmonyPatch(typeof(BattleTech.Mech))]
+        [HarmonyPatch("AddInstability")]
+        public static class BattleTech_Pilot_CanPilot_Prefix
+        {
+            static void Prefix(Mech __instance, float amt, StabilityChangeSource source, string sourceGuid)
+            {
+                if (amt > 0f)
+                {
+                    float currentStability = __instance.CurrentStability + amt;
+                    float maxStability = __instance.MaxStability;
+
+                    if(currentStability / maxStability >= 1.25f)
+                    {
+                        //doing 25% stability damage over the maximum will result in a knockdown
+                        __instance.FlagForKnockdown();
+                    }
+                }
+            }
+        }
+    }
+}
